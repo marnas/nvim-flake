e0ccb43a5a75bb967b5dacf26492fbd49631a4db
Marco Santonastaso <marco@santonastaso.com>
2024-11-26 19:44:28 +0000

    buffer management shortcuts updated

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,5 +1,10 @@
 vim.opt.termguicolors = true
+
+-- Close all unfocused buffers
+vim.api.nvim_set_keymap("n", "<C-q>", ":%bdelete|edit#|bdelete#<CR>", {})
+-- Only close focused buffer
 vim.api.nvim_set_keymap("n", "<C-w>", ":bp <cr>:bd# <cr>", {})
+-- Rearrange buffers
 vim.api.nvim_set_keymap("n", "<leader>bl", ":BufferLineMoveNext <cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>bh", ":BufferLineMovePrev <cr>", {})
 
--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -22,6 +22,7 @@ require("neo-tree").setup({
 	-- in whatever position is specified in window.position
 	-- "open_current",  -- netrw disabled, opening a directory opens within the
 	use_libuv_file_watcher = true,
+	-- show_unloaded = true,
 	window = {
 		mappings = {
 			["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
