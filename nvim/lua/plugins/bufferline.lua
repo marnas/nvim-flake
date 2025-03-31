42301a32fdf6e2dc9a57c0db591dd9de5d3ed640
Marco Santonastaso <marco@santonastaso.com>
2025-03-31 15:16:41 +0100

    close buffer keymap updated

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -3,7 +3,7 @@ vim.opt.termguicolors = true
 -- Close all unfocused buffers
 vim.api.nvim_set_keymap("n", "<C-q>", ":%bdelete|edit#|bdelete#<CR>", {})
 -- Only close focused buffer
-vim.api.nvim_set_keymap("n", "<C-w>", ":bp <cr>:bd# <cr>", {})
+vim.api.nvim_set_keymap("n", "<C-w>", ":bp <cr>:bd# <cr>", { nowait = true })
 -- Rearrange buffers
 vim.api.nvim_set_keymap("n", "<leader>bl", ":BufferLineMoveNext <cr>", {})
 vim.api.nvim_set_keymap("n", "<leader>bh", ":BufferLineMovePrev <cr>", {})
